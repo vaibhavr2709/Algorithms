46b4ffbeb00e05e37fe3e8197d6624d3dcdfa63c
Vaibhav Rastogi <40246175+vaibhavr2709@users.noreply.github.com>
2020-03-29 23:59:43 +0530

    Update lca.cpp

--- lca.cpp
+++ lca.cpp
@@ -8,13 +8,13 @@ void find_depth(ll node, ll dep){
     for(auto &x: v[node]){
         if(x==parent[node])
             continue;
+        parent[x]=node;
         find_depth(x,dep+1);
     }
 }
 void find_sparse(ll n){
-    rep1(i,n){
+    rep1(i,n)
         sparse[i][0]=parent[i];
-    }
     for(ll j=1; 1<<j <n; j++){
         rep1(i,n){
             if(sparse[i][j-1] != -1)
@@ -23,28 +23,21 @@ void find_sparse(ll n){
     }
 }
 ll query_lca(ll a, ll b){
-    ll log_of_ht_of_a,l, same_level_anc;
     if(depth[a]<depth[b])
         swap(a,b);
-    for(l=1; 1<<l <= depth[a]; l++);
-    l--;
-    log_of_ht_of_a=l;
-    for(ll i=log_of_ht_of_a; i>=0; i--){
+    for(ll i=20; i>=0; i--)
         if(depth[a]-(1<<i) >= depth[b])
             a = sparse[a][i];
-    }
     if(a==b)
         return a;
-    for(ll i=log_of_ht_of_a; i>=0; i--){
+    for(ll i=20; i>=0; i--)
         if(sparse[a][i] != -1 && sparse[a][i] != sparse[b][i])
             a = sparse[a][i], b = sparse[b][i];
-    }
     return parent[a];
 }
 ll pth_ancestor(ll a, ll p){
-    if(depth[a]<=p){
+    if(depth[a]<=p)
         return -1;
-    }
     ll c=0;
     while(p){
         if(p&1)
