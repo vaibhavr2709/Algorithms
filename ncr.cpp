f330bba95492da0ee9326065f76f67857e675b0f
Vaibhav Rastogi <40246175+vaibhavr2709@users.noreply.github.com>
2020-04-08 00:24:39 +0530

    Update ncr.cpp

--- ncr.cpp
+++ ncr.cpp
@@ -11,12 +11,20 @@ inline vector<ll> allinv(ll n, ll p){
     return arr;
 }
 vector<ll> inline allncr(ll n, ll p){
+    ll facn=1;
     vector<ll> nci(n+1);
     nci[0]=1;
     nci[n]=1;
+    if(n<=1)
+        return nci;
     vector<ll> v = allinv(n,p);
-    fr(i,1,n,1){
-        nci[i]=(nci[i-1]*(n-i-1))%p;
-    }
+    vector<ll> facinv(n+1);
+    facinv[1]=1;
+    for(ll i=2; i<n; i++)
+        facinv[i]=(facinv[i-1]*v[i])%p;
+    rep1(i,n)
+        facn = (facn*i)%p;
+    fr(i,1,n,1)
+        nci[i]=(((facn*facinv[i])%p)*facinv[n-i])%p;
     return nci;
 }
