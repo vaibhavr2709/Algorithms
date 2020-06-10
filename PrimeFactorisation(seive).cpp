245c42e459b58d8bef53e7860da99c0fe88c0faa
Vaibhav Rastogi <40246175+vaibhavr2709@users.noreply.github.com>
2020-06-10 12:17:05 +0530

    Can calculate prime factorisation of N numbers in O(N*log(N)*log(N))
    Get prime factorization of N in O(logN) after seive

---
+++ PrimeFactorisation(seive).cpp
@@ -0,0 +1,31 @@
+// I call sieve as seive. Feels better.
+const ll seiveMaxn = 15000000;
+vector<ll> seive(seiveMaxn+1,0);
+void doSeive(){
+    for(ll i=2; i<=maxn; i++){
+        if(seive[i]==0){
+            seive[i]=i;
+            for(ll j=i*i; j<=maxn; j+=i)
+                seive[j]=i;
+        }
+    }
+}
+
+vector<ll> getPrimeFactorisation(ll x){
+    vector<ll> ans;
+    while(x!=1){
+        ans.pb(seive[x]);
+        x/=seive[x];
+    }
+    return ans;
+}
+
+vector<ll> getPrimeFactors(ll x){
+    vector<ll> ans;
+    while(x!=1){
+        if(ans.size()==0 || ans.back()!=seive[x])
+            ans.pb(seive[x]);
+        x/=seive[x];
+    }   
+    return ans;
+}
